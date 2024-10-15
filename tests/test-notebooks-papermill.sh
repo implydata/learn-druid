1cefeb422929dc3075f353b1440af82e3437cf40
Hugh Evans <hugh.evans.dev@gmail.com>
2024-10-15 17:02:11 +0100

    remove 03-19 from skip list fix missing column

--- tests/test-notebooks-papermill.sh
+++ tests/test-notebooks-papermill.sh
@@ -3,8 +3,7 @@
 bash ./launch-test-environment.sh
 
 skiplList=("notebooks/03-query/11-joins.ipynb" #Hangs forever on users data generation, even when run manually, just broken?
-"notebooks/03-query/19-groupby-earliest.ipynb" #Queries for session length column which has already been filtered out at ingestion?
-"notebooks/99-contributing/notebook-template.ipynb")
+"notebooks/99-contributing/notebook-template.ipynb") #Contains invalid sql
 
 ALL_CHANGED_FILES=$(find notebooks -type f -name '*.ipynb' | awk '!/\/\./')
 for file in ${ALL_CHANGED_FILES}; do
